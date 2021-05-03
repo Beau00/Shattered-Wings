e4d0cf7047f6ab0a33c11844422f45007a7255c4
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2021-05-03 18:47:05 +0200

    test

--- Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Flower System/FlowerSystem.cs
+++ Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Flower System/FlowerSystem.cs
@@ -24,9 +24,11 @@ public class FlowerSystem : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+
+        if(collision.gameObject)
         /* if bunca has collision with one of the flowers
          * play pickup animation, delete flower, instantiate in mouth of bunca
-         * 
+         * OP VOLGORDE
          * if bunca wants to drop, press a key
          * play drop animation, instantiate from mouth to make it fall out. 
          * delete flower in mouth.
