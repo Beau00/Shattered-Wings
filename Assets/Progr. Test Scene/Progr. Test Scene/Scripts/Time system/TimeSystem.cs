fbc81bee1e7a5a76cafabf6b0e8b7aba9085eec8
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2021-05-05 15:08:40 +0200

    smoll time system update

--- Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Time system/TimeSystem.cs
+++ Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Time system/TimeSystem.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class TimeSystem : MonoBehaviour
 {
-    public Material night;
-    public Material day;
-    public Material sunUp;
-    public Material sunDown;
+    public Material night, day, sunUp, sunDown;
+    public Canvas blackOut;
+    public TimeScreenUp screenTime;
    
     void Start()
     {
         RenderSettings.skybox = day;
+        blackOut.enabled = false;
     }
 
     void Update()
@@ -21,8 +21,16 @@ public class TimeSystem : MonoBehaviour
 
     public void Night()
     {
+       // remove the menu
        
+        // sleeping animation
+
+        // wait for seconds       Invoke("SetActive", 5.0f);  Invoke("SetFalse", 5.0f);     where as 5 is the numbers of seconds for the wait of active and non active.
+        blackOut.enabled = true; 
         RenderSettings.skybox = night;
+        // wait for seconds to delete
+
+        //wake up animation and a new day started!
     }
 
     public void Day()
