207fdccf6d8c1547870e4dd684ef8c32485d9a79
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2021-05-23 19:21:13 +0200

    altaar 3 bug fixed

--- Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Graveyard Systems/SwordSystem.cs
+++ Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Graveyard Systems/SwordSystem.cs
@@ -19,24 +19,26 @@ public class SwordSystem : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
        
-            if(collision.transform.name == swordOne.transform.name)
-            {
-                swordOne.transform.position = swordOnePlacement.position;
-                // freeze sword position
-                swordOne.transform.parent = swordOnePlacement;
-                //swordOne.GetComponent<Rigidbody>().useGravity = false;
-                
-            }
+        if(collision.transform.name == swordOne.transform.name)
+        {
+            swordOne.transform.position = swordOnePlacement.position;
+            // freeze sword position
+            swordOne.transform.parent = swordOnePlacement;
+            swordOne.GetComponent<Rigidbody>().useGravity = false;
+            swordOne.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            swordOne.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
         
-            if (collision.transform.name == swordTwo.transform.name)
-            {
-                swordTwo.transform.position = swordTwoPlacement.position;
-                // freeze sword position
-                swordTwo.transform.parent = swordTwoPlacement;
-               // swordTwo.GetComponent<Rigidbody>().useGravity = false;
-            
-                
-            }
+        if (collision.transform.name == swordTwo.transform.name)
+        {
+            swordTwo.transform.position = swordTwoPlacement.position;
+            // freeze sword position
+            swordTwo.transform.parent = swordTwoPlacement;
+            swordTwo.GetComponent<Rigidbody>().useGravity = false;
+            swordTwo.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            swordTwo.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+        }
        
         
     }
