f45673e8d9e2491d50208f6c6ec75ff1e8c62278
Beau00 <70371863+Beau00@users.noreply.github.com>
2021-05-23 11:09:25 +0200

    Main menu update

---
+++ Assets/Main menu/BackButton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButton : MonoBehaviour
+{
+
+    public GameObject settingsCanvas;
+    public GameObject menuCanvas;
+
+    public void Button()
+    {
+        settingsCanvas.SetActive(false);
+        menuCanvas.SetActive(true);
+    }
+}
+
