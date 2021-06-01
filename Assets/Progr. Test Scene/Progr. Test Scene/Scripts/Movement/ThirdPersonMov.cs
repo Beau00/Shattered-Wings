36aca8745cd88ab97888d185d49f8b76a7a48345
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2021-06-01 21:04:34 +0200

    third person movement
    just animation adding and then its done

---
+++ Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Movement/ThirdPersonMov.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMov : MonoBehaviour
+{
+    public CharacterController playerController;
+    public float speed = 6f;
+    public Transform cam;
+    float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(hor, 0, ver).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            playerController.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
