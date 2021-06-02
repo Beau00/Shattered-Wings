fb7ac8621395150e5b2abf19cd03ac436c9e76a0
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2021-06-02 17:28:13 +0200

    movement collider fix
    fixing collider problem hehe

--- Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Movement/PickUp.cs
+++ Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Movement/PickUp.cs
@@ -40,65 +40,20 @@ public class PickUp : MonoBehaviour
         foreach (var hitCollider in hitColliders)
         {
 
-
-
-            // for all flowers / bones + animation
-
-            if (hitCollider.transform.name.Equals(axe1.transform.name) && Input.GetButtonDown("E"))
+            if (hitCollider.transform.tag.Equals("item") && Input.GetButtonDown("E"))
             {
                 Debug.Log("Flower One Check");
-
 
                 axe1.GetComponent<Rigidbody>().useGravity = false;
                 axe1.transform.position = pickUpPosition.transform.position;
                 axe1.transform.parent = GameObject.Find("PickUpPosition").transform;
                 // flowerOneRB.constraints = RigidbodyConstraints.FreezePosition; // doesnt update. fix
-
             }
             if (Input.GetButtonUp("E"))
             {
                 axe1.transform.parent = null;
                 axe1.GetComponent<Rigidbody>().useGravity = true;
             }
-
-
-
-            if (hitCollider.transform.name == axe2.transform.name && Input.GetButtonDown("E"))
-            {
-                Debug.Log("Flower Two Check");
-
-
-                axe2.GetComponent<Rigidbody>().useGravity = false;
-                axe2.transform.position = pickUpPosition.transform.position;
-                axe2.transform.parent = GameObject.Find("PickUpPosition").transform;
-                // flowerOneRB.constraints = RigidbodyConstraints.FreezePosition; // doesnt update. fix
-
-            }
-            if (Input.GetButtonUp("E"))
-            {
-                axe2.transform.parent = null;
-                axe2.GetComponent<Rigidbody>().useGravity = true;
-            }
-
-
-            if (hitCollider.transform.name == axe3.transform.name && Input.GetButtonDown("E"))
-            {
-                Debug.Log("Flower Two Check");
-
-
-                axe3.GetComponent<Rigidbody>().useGravity = false;
-                axe3.transform.position = pickUpPosition.transform.position;
-                axe3.transform.parent = GameObject.Find("PickUpPosition").transform;
-                // flowerOneRB.constraints = RigidbodyConstraints.FreezePosition; // doesnt update. fix
-
-            }
-            if (Input.GetButtonUp("E"))
-            {
-                axe3.transform.parent = null;
-                axe3.GetComponent<Rigidbody>().useGravity = true;
-            }
-
-
             
         }
     }
--- Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Movement/ThirdPersonMov.cs
+++ Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Movement/ThirdPersonMov.cs
@@ -9,6 +9,9 @@ public class ThirdPersonMov : MonoBehaviour
     public Transform cam;
     float turnSmoothVelocity;
     public float turnSmoothTime = 0.1f;
+    bool isGrounded;
+    Vector3 moveVector;
+    float verticalVelosity;
 
     // Start is called before the first frame update
     void Start()
@@ -22,15 +25,32 @@ public class ThirdPersonMov : MonoBehaviour
         float hor = Input.GetAxisRaw("Horizontal");
         float ver = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(hor, 0, ver).normalized;
+        
+        isGrounded = playerController.isGrounded;
 
-        if(direction.magnitude >= 0.1f)
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                playerController.Move(moveDir.normalized * speed * Time.deltaTime);
+            }
+
+        moveVector = new Vector3(0, verticalVelosity, 0);
+        playerController.Move(moveVector);
+        if (isGrounded)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            playerController.Move(moveDir.normalized * speed * Time.deltaTime);
+            verticalVelosity -= 0;
         }
+        else
+        {
+            verticalVelosity -= 1;
+        }
+
+       
+
+       
     }
 }
