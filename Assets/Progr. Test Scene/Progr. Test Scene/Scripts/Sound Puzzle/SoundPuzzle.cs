5fbebe5e2073e054797f993cf369d8dcf7afaa0b
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2021-06-04 20:43:00 +0200

    new materials and smoll altaar 2 updates
    some new materials and smoll updates

--- Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Sound Puzzle/SoundPuzzle.cs
+++ Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Sound Puzzle/SoundPuzzle.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class SoundPuzzle : MonoBehaviour
 {
-    public AudioSource lowest, lower, low, middle, high, higher, highest;
+    public AudioSource lower, low, high, higher;
 
     public GameObject smollDoorOne, smollDoorTwo, smollDoorThree, smollDoorFour;
     public bool smolldoorOpenOne, smolldoorOpenTwo, smolldoorOpenThree, smolldoorOpenFour;
     public float doorRotation;
     public GameObject trofeeSoundPuzzle;
-    LinkedList<AudioSource> input = new LinkedList<AudioSource>(); 
-    LinkedList<AudioSource> solution = new LinkedList<AudioSource>();
+
+  
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        solution.AddLast(lower); // volgorde van bepaalde sounds
-        solution.AddLast(middle);
-        solution.AddLast(high);
-        solution.AddLast(lowest);
+        
     }
-
-    // Update is called once per frame
     void Update()
     {
         
     }
+  
+
+
 }
