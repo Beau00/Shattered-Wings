064dc328555ea9c4746054d79e5fa0972ae8c200
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2021-06-19 15:02:36 +0200

    altaar 2 smol change en altaar 3 ook

--- Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Sound Puzzle/SoundPuzzle.cs
+++ Assets/Progr. Test Scene/Progr. Test Scene/Scripts/Sound Puzzle/SoundPuzzle.cs
@@ -9,17 +9,17 @@ public class SoundPuzzle : MonoBehaviour
     public Transform doorOneCollider, doorTwoCollider, doorThreeCollider, doorFourCollider;
 
     public Animator door1, door2, door3, door4;
-    public Animation d1,d2,d3,d4;
+    public Animation d1, d2, d3, d4;
 
     //public GameObject smollDoorOne, smollDoorTwo, smollDoorThree, smollDoorFour;
     public bool smolldoorOpenOne = false, smolldoorOpenTwo = false, smolldoorOpenThree = false, smolldoorOpenFour = false;
-    
+
     public float doorRotation;
     public GameObject trofeeSoundPuzzle;
 
     public GameObject player;
     public float delay;
-    
+
     // animation add && main item systemm
 
     // Start is called before the first frame update
@@ -27,12 +27,12 @@ public class SoundPuzzle : MonoBehaviour
     {
         door1.SetBool("C1", true);
         door2.SetBool("C2", true);
-        door4.SetBool("C3", true);
-        door3.SetBool("C4", true);
+        door3.SetBool("C3", true);
+        door4.SetBool("C4", true);
 
-        lower.Stop();  
+        lower.Stop();
         low.Stop();
-        high.Stop();   
+        high.Stop();
         higher.Stop();
     }
 
@@ -43,7 +43,7 @@ public class SoundPuzzle : MonoBehaviour
         {
             if (collider1.transform.name == player.transform.name && Input.GetButtonDown("E"))
             {
-                lower.PlayDelayed(0.5f);
+                lower.PlayDelayed(1.2f);
                 smolldoorOpenOne = true;
                 delay = Time.time;
                 door1.SetBool("Open", true);
@@ -51,11 +51,14 @@ public class SoundPuzzle : MonoBehaviour
                 door1.SetBool("OpenClose", false);
 
             }
-            else if(smolldoorOpenOne = true && Input.GetButtonDown("F"))
+            else if (smolldoorOpenOne = true && Input.GetButtonDown("F"))
             {
-                lower.Stop();
+
                 smolldoorOpenOne = false;
                 delay = Time.time;
+
+                StartCoroutine(DelayedSoundStopOne());
+
                 door1.SetBool("OpenClose", true);
                 door1.SetBool("CloseOpen", false);
                 door1.SetBool("Open", false);
@@ -67,18 +70,21 @@ public class SoundPuzzle : MonoBehaviour
         {
             if (collider2.transform.name == player.transform.name && Input.GetButtonDown("E"))
             {
-                low.PlayDelayed(0.5f);
+                low.PlayDelayed(1.2f);
                 smolldoorOpenTwo = true;
                 delay = Time.time;
                 door2.SetBool("DoorOpen", true);
                 door2.SetBool("DoorCloseOpen", true);
                 door2.SetBool("DoorClose", false);
             }
-            else if(smolldoorOpenTwo = true && Input.GetButtonDown("F"))
+            else if (smolldoorOpenTwo = true && Input.GetButtonDown("F"))
             {
-                low.Stop();
+
                 smolldoorOpenTwo = false;
                 delay = Time.time;
+
+                StartCoroutine(DelayedSoundStopTwo());
+
                 door2.SetBool("DoorClose", true);
                 door2.SetBool("DoorOpen", false);
                 door2.SetBool("DoorCloseOpen", false);
@@ -86,24 +92,26 @@ public class SoundPuzzle : MonoBehaviour
         }
 
         Collider[] collidersThree = Physics.OverlapSphere(doorThreeCollider.transform.position, 0.5f);
-        foreach (Collider collider3 in collidersThree )
+        foreach (Collider collider3 in collidersThree)
         {
-          
+
             if (collider3.transform.name == player.transform.name && Input.GetButtonDown("E"))
             {
-                high.PlayDelayed(0.5f);
+                high.PlayDelayed(1.2f);
                 smolldoorOpenThree = true;
                 delay = Time.time;
                 door3.SetBool("OpenD", true);
                 door3.SetBool("CloseD", false);
                 door3.SetBool("CloseDOpenD", true);
             }
-            else if(smolldoorOpenThree = true && Input.GetButtonDown("F"))
+            else if (smolldoorOpenThree = true && Input.GetButtonDown("F"))
             {
-                high.Stop();
+
                 smolldoorOpenThree = false;
                 delay = Time.time;
-                
+
+                StartCoroutine(DelayedSoundStopThree());
+
                 door3.SetBool("CloseD", true);
                 door3.SetBool("OpenD", false);
                 door3.SetBool("CloseDOpenD", false);
@@ -113,34 +121,56 @@ public class SoundPuzzle : MonoBehaviour
         Collider[] collidersFour = Physics.OverlapSphere(doorFourCollider.transform.position, 0.5f);
         foreach (Collider collider4 in collidersFour)
         {
-            
+
             if (collider4.transform.name == player.transform.name && Input.GetButtonDown("E"))
             {
-                higher.PlayDelayed(0.5f);
+                higher.PlayDelayed(1.2f);
                 smolldoorOpenFour = true;
                 delay = Time.time;
                 door4.SetBool("OpenTrue", true);
                 door4.SetBool("OpenToClose", false);
                 door4.SetBool("CloseToOpen", true);
             }
-            else if(smolldoorOpenFour = true && Input.GetButtonDown("F"))
+            else if (smolldoorOpenFour = true && Input.GetButtonDown("F"))
             {
-                higher.Stop();
+
                 smolldoorOpenFour = false;
                 delay = Time.time;
+
+                StartCoroutine(DelayedSoundStopFour());
+
                 door4.SetBool("OpenToClose", true);
-                door4.SetBool("OpenTrue", false) ;
+                door4.SetBool("OpenTrue", false);
                 door4.SetBool("CloseToOpen", false);
 
 
             }
         }
 
-   
+
+        IEnumerator DelayedSoundStopFour()
+        {
+            yield return new WaitForSeconds(1.2f);
+            higher.Stop();
+        }
+        IEnumerator DelayedSoundStopThree()
+        {
+            yield return new WaitForSeconds(1.2f);
+            high.Stop();
+        }
+        IEnumerator DelayedSoundStopTwo()
+        {
+            yield return new WaitForSeconds(1.2f);
+            low.Stop();
+        }
+        IEnumerator DelayedSoundStopOne()
+        {
+            yield return new WaitForSeconds(1.2f);
+            lower.Stop();
+        }
+
+
+
+
     }
-
-  
-  
-
-
 }
