9794e203243a4e60e97b916e52a1d6697e5a7141
Beau00 <70371863+Beau00@users.noreply.github.com>
2021-06-22 12:16:16 +0200

    fadie in menu
    sadfsdfas

--- Assets/Main menu/Code Beau/Fadie.cs
+++ Assets/Main menu/Code Beau/Fadie.cs
@@ -2,25 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Fadie : MonoBehaviour
 {
     
     public Image ade;
-    public float alpha = 0;
+    public bool startEnd;
+    public GameObject image;
+    public float time;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
+        image.SetActive(true);
+        ade.canvasRenderer.SetAlpha(0f);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        ade.color = new Color(0, 0, 0, alpha) * Time.deltaTime;
 
-        alpha++;
+    
+    public void FadeIn()
+    {
+        ade.CrossFadeAlpha(1, 1, false);
+        startEnd = true;
+        StartCoroutine(Henk());
+    }
+
+    public void FadeOut()
+    {
+        ade.CrossFadeAlpha(0, 1, false);
+    }
+
+   IEnumerator Henk()
+    {
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene(1);
 
     }
+
+
+
+
+
+
+
+
 }
+
